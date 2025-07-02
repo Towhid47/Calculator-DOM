bdc005b02aaae5aede7b4a1d6d73a14bbf1f9f16
Towhid47 <mdtowhidurrahman47@gmail.com>
2025-07-02 23:46:51 +0600

    Calculator Functioning Working

--- calculator.js
+++ calculator.js
@@ -1,2 +1,40 @@
+// taking input screen of a calculator
+const display = document.getElementById("display");
+
+console.log(display);
+ 
+function takeNumberOrSymbol(value){
+    display.value += value; 
+
+    // if clicking buttons multiple times and taking value exceeding the screen size limit.then,
+    if(display.value.length > 16){
+        display.value = "Out Of Range";
+    }
 
 
+}
+
+function allClear(){
+    display.value = "" ; 
+}
+
+
+function calculate(){
+    let result = eval(display.value);
+
+
+    //if the result is integer, then 
+    if(result%1 == 0){
+        display.value = result ; 
+    }
+    else{
+        display.value = result.toFixed(5)
+    }
+
+    
+}
+
+
+function remove(){
+    display.value = display.value.slice(0,-1); 
+}
